91572a0e922e270fffc9dc979f6c1d83c55ab3be
A3Tom <Tom_Esq@Yahoo.com>
2020-03-30 00:49:25 +0100

    Added ability to connect and disconnect to specific devices via bluetooth... Another step closer to greatness

--- Client/ShinyPrismBLE_Trial/ShinyPrismBLE_Trial/Models/FoundPeripheralVM.cs
+++ Client/ShinyPrismBLE_Trial/ShinyPrismBLE_Trial/Models/FoundPeripheralVM.cs
@@ -4,8 +4,14 @@ namespace ShinyPrismBLE_Trial.Models
 {
     public class FoundPeripheralVM
     {
-        public string Name { get; set; }
 
-        public IPeripheral Hing { get; set; }
+        public FoundPeripheralVM(IPeripheral newPeripheral)
+        {
+            Hing = newPeripheral;
+        }
+
+        public IPeripheral Hing { get; }
+
+        public string HingName => !string.IsNullOrEmpty(Hing.Name) ? Hing.Name : Hing.Uuid.ToString();
     }
 }
--- Client/ShinyPrismBLE_Trial/ShinyPrismBLE_Trial/Views/MainPage.xaml.cs
+++ Client/ShinyPrismBLE_Trial/ShinyPrismBLE_Trial/Views/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Shiny.BluetoothLE.Central;
+using ShinyPrismBLE_Trial.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,47 +15,69 @@ namespace ShinyPrismBLE_Trial.Views
     public partial class MainPage : ContentPage
     {
         private ICentralManager _central;
+        private ObservableCollection<FoundPeripheralVM> uniquePeripherals = new ObservableCollection<FoundPeripheralVM>();
+        private FoundPeripheralVM selectedPeripheral;
+
         public MainPage(ICentralManager central)
         {
             _central = central;
 
 
             InitializeComponent();
-            lblOutput.Text = "";
             btnSend.Clicked += BtnSend_Clicked;
-
+            btnConnect.Clicked += BtnConnect_Clicked;
+            lvDeviceView.ItemsSource = uniquePeripherals;
+            lvDeviceView.ItemSelected += NewItemSelected;
         }
 
         private async void BtnSend_Clicked(object sender, EventArgs e)
         {
-            var resultSet = new List<IPeripheral>();    
-
             await OutputScan();
 
             btnSend.Text = "Anotha one ?";
-            lblOutput.Text += txtMessage.Text;
+        }
+
+        private async void BtnConnect_Clicked(object sender, EventArgs e)
+        {
+            btnConnect.IsEnabled = false;
+
+            if (selectedPeripheral.Hing.IsConnected())
+            {
+                selectedPeripheral.Hing.CancelConnection();
+                await DisplayAlert("Disconnected device", $"You have disconnected from {selectedPeripheral.HingName}", "Sound");
+            }
+            else
+            {
+                selectedPeripheral.Hing.Connect();
+                await DisplayAlert("New connected device", $"You are now connected to {selectedPeripheral.HingName}", "Cheers");
+            }
+
+
+            btnConnect.IsEnabled = true;
         }
 
         private async Task OutputScan()
         {
             await _central.RequestAccess();
 
-            lblOutput.Text += $"using ble adapter : {_central.AdapterName}, Can control: {_central.CanControlAdapterState()}";
-
-            var test2 = await _central.ScanForUniquePeripherals()
-                .Select(x => lblOutput.Text += $"{Environment.NewLine}Just foon : {GetPeripheralVisibleAlias(x.Name, x.Uuid.ToString())}")
-                //.FirstAsync(x => x.Name == "Tams IR Sensor")
+            _central.ScanForUniquePeripherals()
+                .Subscribe(onNext: x => {
+                    uniquePeripherals.Add(new FoundPeripheralVM(x));
+                })
             ;
-
-            //test2.Connect();
 
             //var whitthefuck = await test2.Notify(Guid.NewGuid(), Guid.NewGuid());
                     //new Guid("af0cdaa1-f061-41f5-a9a5-25784b8de258"),
                     //new Guid("7f5e6c0d-4bd6-463c-9f0d-38f429b1b657"));
-
-            swToggle.IsToggled = true;
         }
 
-        private string GetPeripheralVisibleAlias(string peripheralName, string uuid) => !string.IsNullOrEmpty(peripheralName) ? peripheralName : uuid;
+        void NewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            selectedPeripheral = e.SelectedItem as FoundPeripheralVM;
+
+            btnConnect.Text = selectedPeripheral.Hing.IsConnected() ?
+                "Disconnect" :
+                "Connect" ;
+        }
     }
 }
